55ddf80b7789242d6af24151bb8ea9ba67265189
saytkhanov <saythanov2014@mail.ru>
2024-03-23 16:35:03 +0300

    add user story

--- App.js
+++ App.js
@@ -6,23 +6,90 @@
  */
 
 import React from 'react';
-import { SafeAreaView, TouchableOpacity, View, Text } from 'react-native';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  View,
+  Text,
+  FlatList,
+} from 'react-native';
 import Title from './components/Title/Title';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import globalStyle from './assets/styles/globalStyle';
+import UserStory from './components/UserStory/UserStory';
 
 const App = () => {
+  const userStories = [
+    {
+      firstName: 'Joseph',
+      id: 1,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Angel',
+      id: 2,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'White',
+      id: 3,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Olivier',
+      id: 4,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Nata',
+      id: 5,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Nicolas',
+      id: 6,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Nino',
+      id: 7,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Nana',
+      id: 8,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Adam',
+      id: 9,
+      profileImage: require('./assets/images/default_profile.png'),
+    },
+  ];
   return (
     <SafeAreaView>
       <View style={globalStyle.header}>
-        <Title title={'Let’s Explore'} />
+        <Title title={'Getagram'} />
         <TouchableOpacity style={globalStyle.messageIcon}>
           <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
           <View style={globalStyle.messageNumberContainer}>
             <Text style={globalStyle.messageNumber}>2</Text>
           </View>
         </TouchableOpacity>
+      </View>
+      <View style={globalStyle.userStoryContainer}>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          data={userStories}
+          renderItem={({ item }) => (
+            <UserStory
+              firstName={item.firstName}
+              profileImage={item.profileImage}
+            />
+          )}
+        />
       </View>
     </SafeAreaView>
   );
