67b7ddaec032cd0a4d09f5f22bea4ede74b0bf3b
saytkhanov <saythanov2014@mail.ru>
2024-03-23 13:33:18 +0300

    add title component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'plugin:prettier/recommended'],
+  plugins: ['import'],
+  rules: {
+    indent: 'off',
+    'react-native/no-inline-styles': 'off',
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+      },
+    ],
+  },
 };
--- App.js
+++ App.js
@@ -7,9 +7,14 @@
 
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import Title from './components/Title/Title';
 
 const App = () => {
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView>
+      <Title title={'Let’s Explore'} />
+    </SafeAreaView>
+  );
 };
 
 export default App;
